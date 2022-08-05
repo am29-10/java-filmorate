e252c82b3a7904c6d1e0c4c7df2ad787ad75da52
Artur <am29-10@mail.ru>
2022-08-05 12:59:37 +0300

    feat: Добавить ветку с реализацией решения ТЗ 9

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Film {
+
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime releaseDate;
+    private long duration;
+}
