d036ada7ad1edcee77eef23beabe552deee8db11
Artur <am29-10@mail.ru>
2022-08-08 12:43:44 +0300

    Перенести с контроллера всю логику в сервис вместе с логами

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
@@ -1,45 +1,77 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.NoMovieFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class FilmServiceImpl implements FilmService {
 
     private static final Map<Integer, Film> films = new HashMap<>();
     private static int id;
+    private static final LocalDate birthdayMovie = LocalDate.of(1895,12,28);
+    private static final int limitDescription = 200;
 
     @Override
-    public void create(Film film) {
+    public Film create(Film film) throws ValidationException {
         if (film.getId() == 0) {
             film.setId(++id);
         }
+        validate(film);
         films.put(film.getId(), film);
+        log.info("Фильм с id '{}' добавлен в список", film.getId());
+        return film;
     }
 
     @Override
     public Map<Integer, Film> readAll() {
+        log.info("Количество имеющихся фильмов: {}", films.size());
         return films;
     }
 
-    @Override
-    public Film read(int id) {
-        return films.get(id);
-    }
 
     @Override
-    public void update(Film film) {
+    public Film update(Film film) throws ValidationException, NoMovieFoundException {
         if (films.containsKey(film.getId())) {
+            validate(film);
             films.put(film.getId(), film);
+            log.info("Фильм с id '{}' обновлен", film.getId());
+            return film;
+        } else {
+            log.info("NoMovieFoundException (Фильм не может быть обновлен, т.к. его нет в списке)");
+            throw new NoMovieFoundException("Фильм не может быть обновлен, т.к. его нет в списке");
         }
     }
 
-    @Override
-    public void delete(int id) {
-        films.remove(id);
+
+    public void validate(Film film) throws ValidationException {
+        if (film.getName().isEmpty() || film.getName().isBlank()) {
+            log.info("ValidationException (Пустое название фильма)");
+            throw new ValidationException("Пустое название фильма");
+        }
+        if (film.getDescription().length() > limitDescription) {
+            log.info("ValidationException (Выход за пределы длины описания фильма)");
+            throw new ValidationException("Выход за пределы длины описания фильма");
+        }
+        if (film.getReleaseDate().isBefore(birthdayMovie)) {
+            log.info("ValidationException (Дата релиза раньше дня рождения кино)");
+            throw new ValidationException("Дата релиза раньше дня рождения кино");
+        }
+        if (film.getDuration() < 0) {
+            log.info("ValidationException (Продолжительность фильма имеет отрицательное значение)");
+            throw new ValidationException("Продолжительность фильма имеет отрицательное значение");
+        }
+        if (film.getId() < 0) {
+            log.info("ValidationException (Значение id не может быть отрицательным)");
+            throw new ValidationException("Значение id не может быть отрицательным");
+        }
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
@@ -1,46 +1,74 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.exceptions.NoMovieFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private static final Map<Integer, User> users = new HashMap<>();
     private static int id;
 
     @Override
-    public void create(User user) {
+    public User create(User user) throws ValidationException {
         if (user.getId() == 0) {
             user.setId(++id);
         }
+        validate(user);
         users.put(user.getId(), user);
+        log.info("Фильм с id '{}' добавлен в список", user.getId());
+        return user;
     }
 
     @Override
     public Map<Integer, User> readAll() {
+        log.info("Количество имеющихся фильмов: {}", users.size());
         return users;
+
     }
 
     @Override
-    public User read(int id) {
-        return users.get(id);
-    }
-
-    @Override
-    public void update(User user) {
+    public User update(User user) throws ValidationException, NoMovieFoundException {
         if (users.containsKey(user.getId())) {
+            validate(user);
             users.put(user.getId(), user);
+            log.info("Фильм с id '{}' обновлен", user.getId());
+            return user;
+        } else {
+            log.info("NoMovieFoundException (Фильм не может быть обновлен, т.к. его нет в списке)");
+            throw new NoMovieFoundException("Фильм не может быть обновлен, т.к. его нет в списке");
         }
     }
 
-    @Override
-    public void delete(int id) {
-        users.remove(id);
+    public void validate(User user) throws ValidationException {
+        if (user.getEmail().isEmpty() || user.getEmail().isBlank() || !(user.getEmail().contains("@"))) {
+            log.info("ValidationException (Пустой email или email не содержит '@')");
+            throw new ValidationException("Пустой email или email не содержит '@'");
+        }
+        if (user.getLogin().isEmpty() || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.info("ValidationException (Пустой логин или логин содержит пробелы)");
+            throw new ValidationException("Пустой логин или логин содержит пробелы");
+        }
+        if (user.getName().isEmpty() || user.getName().isBlank()) {
+            log.info("Имя пользователя соответствует его логину");
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.info("ValidationException (Дата рождения указана в будущем времени)");
+            throw new ValidationException("Дата рождения указана в будущем времени");
+        }
+        if (user.getId() < 0) {
+            log.info("ValidationException (Значение id не может быть отрицательным)");
+            throw new ValidationException("Значение id не может быть отрицательным");
+        }
     }
 }
 
