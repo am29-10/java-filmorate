1a3bba2261add21020f9d23a9ef530529458d1d0
Artur <am29-10@mail.ru>
2022-08-09 00:49:19 +0300

    Добавить один класс exception вместо двух, которые выполняют одно и то же условие

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NoMovieFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class NoMovieFoundException extends Throwable {
-    public NoMovieFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NoUserFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class NoUserFoundException extends Throwable {
-    public NoUserFoundException(String message) {
-        super(message);
-    }
-}
