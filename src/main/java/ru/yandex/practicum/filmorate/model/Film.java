755c967af7e69c4cb74bbc25981ef4ead5df21b2
Artur <am29-10@mail.ru>
2022-08-18 20:24:34 +0300

    Добавить список лайков

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,11 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Film {
 
     private int id;
@@ -13,5 +18,5 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
-    private Set<Integer> likes;
+    private Set<Integer> likes = new HashSet<>();
 }
