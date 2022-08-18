2839459c729e3ba6fc55ec27fb7376ed1e87e052
Artur <am29-10@mail.ru>
2022-08-18 13:08:14 +0300

    Добавить новую логику, связанную с лайками фильма

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,17 +1,55 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.EntityNotFoundException;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public interface FilmService {
+@Service
+public class FilmService {
+    private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
+    @Autowired
+    public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
+        this.filmStorage = filmStorage;
+        this.userStorage = userStorage;
+    }
 
-    Film create(Film film) throws ValidationException;
+    public Film create(Film film) {
+        return filmStorage.create(film);
+    }
+    public Map<Integer, Film> readAll() {
+        return filmStorage.readAll();
+    }
 
-    Map<Integer, Film> readAll() throws EntityNotFoundException;
+    public Film update(Film film) {
+        return filmStorage.update(film);
+    }
+    public void addLike(int userId, int filmId) {
+        if (userStorage.readAll().containsKey(userId) && readAll().containsKey(filmId)) {
+            readAll().get(filmId).getLikes().add(userId);
+        } else {
+            throw new EntityNotFoundException("Объект не найден. Необходимо проверить id");
+        }
+    }
 
-    Film update(Film film) throws ValidationException, EntityNotFoundException;
+    public void removeLike(int userId, int filmId) {
+        if (userStorage.readAll().containsKey(userId) && readAll().containsKey(filmId)) {
+            readAll().get(filmId).getLikes().remove(userId);
+        } else {
+            throw new EntityNotFoundException("Объект не найден. Необходимо проверить id");
+        }
+    }
 
+    public List<Film> getTop10Films() {
+        return readAll().values().stream()
+                .sorted(Comparator.comparing(film -> film.getLikes().size(), Comparator.reverseOrder()))
+                .limit(10)
+                .collect(Collectors.toList());
+    }
 }
