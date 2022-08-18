fcd72dee0ade06359ef154ba36e12381b30feac2
Artur <am29-10@mail.ru>
2022-08-18 20:28:14 +0300

    Добавить новые эндпоинты, связанные с лайками

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,35 +1,68 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.EntityNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/films")
 public class FilmController {
     private final FilmService filmService;
 
+    @Autowired
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
 
+    @GetMapping("/{id}")
+    public Film get(@PathVariable int id) {
+        log.info("Получен запрос GET /films/{}", id);
+        return filmService.getFilmById(id);
+    }
+
     @GetMapping
-    public List<Film> getAll() throws EntityNotFoundException {
+    public List<Film> getAll() {
+        log.info("Получен запрос GET /films");
         return new ArrayList<>(filmService.readAll().values());
     }
 
     @PostMapping
-    public Film create(@RequestBody Film film) throws ValidationException {
+    public Film create(@RequestBody Film film) {
+        log.info("Получен запрос POST /films");
         return filmService.create(film);
     }
 
     @PutMapping
-    public Film update(@RequestBody Film film) throws ValidationException, EntityNotFoundException {
+    public Film update(@RequestBody Film film) {
+        log.info("Получен запрос PUT /films");
         return filmService.update(film);
     }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable int id, @PathVariable int userId) {
+        log.info("Получен запрос PUT /films/{}/like/{}", id, userId);
+        filmService.addLike(userId, id);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLike(@PathVariable int id, @PathVariable int userId) {
+        log.info("Получен запрос DELETE /films/{}/like/{}", id, userId);
+        filmService.removeLike(userId, id);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
+        log.info("Получен запрос GET /films/popular");
+        return filmService.getPopularFilms(count);
+    }
+
 }
