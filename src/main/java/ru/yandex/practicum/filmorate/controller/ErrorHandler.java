22af4553e2ea3160ecd10341fa2105a47f9a6714
Artur <am29-10@mail.ru>
2022-08-30 18:53:37 +0300

    Добавить обработку исключения реализующую ответ 500

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -26,4 +26,11 @@ public class ErrorHandler {
         log.info("Объект не найден");
         return new ErrorResponse(e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        log.info("500 {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
 }
